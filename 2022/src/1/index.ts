228adec4e84426d5e1544204a2809d93023d3e3b
Rui Guedes <eng.ruiguedes@gmail.com>
2022-12-06 16:44:46 +0000

    Solved the 1st challenge from 2022

---
+++ 2022/src/1/index.ts
@@ -0,0 +1,18 @@
+import { syncReadFile } from '../utils';
+
+function solve() {
+  const input = syncReadFile('1/input.in').split('\n');
+
+  const data = input.reduce<number[]>((acc, val) => {
+    if (acc.length === 0) acc.push(Number.parseInt(val));
+    else if (val === '') acc.push(0);
+    else acc[acc.length - 1] = acc[acc.length - 1] + Number.parseInt(val);
+
+    return acc;
+  }, []);
+
+  const output = data.reduce<[number, number]>((acc, val, index) => (acc[1] < val ? [index, val] : acc), [-1, -1]);
+  console.log(`The Elf number ${output[0]} carrying the most calories (${output[1]})`);
+}
+
+solve();
---
+++ 2022/src/utils.ts
@@ -0,0 +1,8 @@
+import fs from 'fs';
+
+/**
+ * Reads the contents of an utf-8 file
+ */
+export function syncReadFile(filename: string) {
+  return fs.readFileSync(`${__dirname}/${filename}`, { encoding: 'utf-8' });
+}
