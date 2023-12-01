6f9d3249b1709f184b24d7f51b9d68bdfb276f32
RuiGuedes <eng.ruiguedes@gmail.com>
2023-12-01 22:32:11 +0000

    chore: setup repository for the 2023 edition

---
+++ 2023/src/01/main.zig
@@ -0,0 +1,12 @@
+//! Advent of Code - Day 1: Trebuchet?!
+//! https://adventofcode.com/2023/day/1
+
+const std = @import("std");
+
+// zig build -Dday=01 run
+pub fn main() !void {
+    std.debug.print("Day 1: Trebuchet?!.\n", .{});
+}
+
+// zig build -Dday=01 test
+test "simple test" {}
